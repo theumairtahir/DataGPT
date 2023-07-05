850b7f995d4d4239cb4cc430938bc9d002f1bbe4
Umair tahir <umairtahir.sparkers@gmail.com>
2023-07-05 19:24:39 +0500

    added schema fetcher

---
+++ src/DataGPT.Abstractions/Data/ISchemaFetcher.cs
@@ -0,0 +1,7 @@
+﻿using DataGPT.Abstractions.Types.Models;
+
+namespace DataGPT.Abstractions.Data;
+public interface ISchemaFetcher
+{
+	public List<DbTable> GetSchema( );
+}
---
+++ src/DataGPT.Abstractions/Types/Models/DbTable.cs
@@ -0,0 +1,13 @@
+﻿namespace DataGPT.Abstractions.Types.Models;
+public class DbTable
+{
+	public required string ObjectId { get; set; }
+	public required string Name { get; set; }
+	public bool IsView { get; set; }
+}
+
+public class DbColumn
+{
+	public required string Name { get; set; }
+	public required string DataType { get; set; }
+}
