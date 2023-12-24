426ba629580ce2e51aa1c8bf5c0cc367e7817bfc
Umair tahir <theumairtahir@gmail.com>
2023-12-24 11:11:01 +0500

    Restapi implementation (#10)
    * added data gpt middleware
    
    * add changes

---
+++ src/DataGPT.RestApi/Extensions/ProcessQueryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DataGPT.Net.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace DataGPT.Net.RestApi.Extensions;
+
+public static class ProcessQueryExtensions
+{
+	public static RouteHandlerBuilder MapDataGpt(this IEndpointRouteBuilder appBuilder)
+	{
+		return appBuilder.MapGet("/datagpt", async ([FromQuery] string query, IQueryProcessingService queryService) => await queryService.ProcessAsync(query)).WithName("DataGPT");
+	}
+}
